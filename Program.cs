df82de012d48dcadae3aa3c8c88d88063423945c
ialikh72 <ialikh.w1@gmail.com>
2026-02-28 00:00:24 +0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+struct Student { public int Id; public string Name; }
+
+class Program
+{
+    static void Main()
+    {
+        Student[] s = new Student[2];
+        s[0] = new Student { Id = 1, Name = "Alex" };
+        s[1] = new Student { Id = 2, Name = "Sam" };
+
+        for (int i = 0; i < s.Length; i++)
+            Console.WriteLine($"{s[i].Id}: {s[i].Name}");
+    }
+}
